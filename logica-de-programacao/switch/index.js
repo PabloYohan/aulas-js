c6428b9a9b9e433f571f9ae723eadec68c95f335
Pablo Yohan <pabloyohandias@gmail.com>
2022-03-03 23:21:57 -0300

    Object Date e Swicth/Case

---
+++ logica-de-programacao/switch/index.js
@@ -0,0 +1,50 @@
+const data = new Date('1987-04-21 00:00:00');
+const diaSemana = data.getDay();
+
+// if (diaSemana === 0) {
+//     diaSemanaTexto = 'Domingo';
+// } else if (diaSemana === 1) {
+//     diaSemanaTexto = 'Segunda';
+// } else if (diaSemana === 2) {
+//     diaSemanaTexto = 'Terça';
+// } else if (diaSemana === 3) {
+//     diaSemanaTexto = 'Quarta';
+// } else if (diaSemana === 4) {
+//     diaSemanaTexto = 'Quinta';
+// } else if (diaSemana === 5) {
+//     diaSemanaTexto = 'Sexta';
+// } else {
+//     diaSemanaTexto = 'Sabado';
+// }
+
+function getDayName(diaSemana) {
+    let diaSemanaTexto;
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            break;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            break;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            break;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            break;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            break;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            break;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            break;
+        default:
+            diaSemanaTexto = '';
+            break;
+    }
+    return diaSemanaTexto;
+}
+console.log(diaSemana, getDayName(diaSemana));
