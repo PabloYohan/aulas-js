d201b07bb6c42faa7571d10261dd918d587e12f7
Pablo Yohan <pabloyohandias@gmail.com>
2022-03-24 20:26:12 -0300

    Estudos de atribuição via desestruturação

---
+++ logica-de-programacao/desestruturacao/index.js
@@ -0,0 +1,7 @@
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const [primeiroNumero, , terceiroNumero, , quintoNumero, , setimoNumero] = numeros;
+// console.log(primeiroNumero, terceiroNumero, quintoNumero, setimoNumero);
+
+const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const numeroSeis = numeros[1][2];
+console.log(numeroSeis);
---
+++ logica-de-programacao/desestruturacao/objetos.js
@@ -0,0 +1,12 @@
+const pessoa = {
+  nome: 'Pablo',
+  sobreNome: 'Yohan',
+  idade: 30,
+  endereco: {
+    rua: 'Noel Barreto',
+    numero: 64
+  }
+};
+
+const { nome } = pessoa;
+console.log(nome);
