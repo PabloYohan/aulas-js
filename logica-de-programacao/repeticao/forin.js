488745843280636c780c6b0e42b4da61e2b049c8
Pablo Yohan <pabloyohandias@gmail.com>
2022-04-16 17:55:47 -0300

    Mais estruturas de repetição usando 'for'

---
+++ logica-de-programacao/repeticao/forin.js
@@ -0,0 +1,15 @@
+// const frutas = ['Pera', 'Maça', 'Uva'];
+
+// for (let i in frutas) {
+//   console.log(frutas[i]);
+// }
+
+const pessoa = {
+  nome: 'Pablo',
+  sobreNome: 'Yohan',
+  idade: 19
+};
+
+for (let i in pessoa) {
+  console.log(pessoa[i]);
+}
---
+++ logica-de-programacao/repeticao/forof.js
@@ -0,0 +1,11 @@
+// const nome = 'Pablo Yohan'
+
+// for (let i of nome) {
+//   console.log(i);
+// };
+
+const frutas = ['Pera', 'Maçã', 'Uva'];
+
+for (let i of frutas) {
+  console.log(i);
+}
